f98cee86c7faee96916cb0e5d996174b7bda18e1
unknown <minto.aminul4@hotmail.com>
2026-09-14 14:16:30 +0200

    Redeploy site

--- src/components/addtechnologie/AddtechToList.tsx
+++ src/components/addtechnologie/AddtechToList.tsx
@@ -4,7 +4,7 @@ const AddtechToList = () => {
     return (
         <div className="container mx-auto my-5">
             <div className=" ml-275 border w-75 h-auto border-amber-100 space-y-5 ">
-                <h2 className="text-[#0F172A] text-center font-bold text-2xl">Your Stack</h2>
+                <h2 className="text-[#0F172A] text-center font-bold text-3xl">Your Stack</h2>
                 <button className="  bg-[#D91B7E] w-full text-xl text-white font-semibold rounded">Remove All</button>
             </div>
         </div>
